0ae4ef2680c75cb1d5f2af309c8539d19297a0c3
lansselot <pozharov2003@gmail.com>
2025-06-06 15:42:59 +0300

    add some basic mw

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
 import express, { Express } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app: Express = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'success' });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
