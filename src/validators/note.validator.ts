14095475ef1fa25fc22655756a0a2583e32f2455
lansselot <pozharov2003@gmail.com>
2025-06-08 03:10:34 +0300

    update validators

--- src/validators/note.validator.ts
+++ src/validators/note.validator.ts
@@ -1,14 +1,35 @@
-import { body } from 'express-validator';
+import { body, param } from 'express-validator';
 
-export const createUserValidator = [
+export const createNoteValidator = [
   body('title')
-    .notEmpty()
-    .withMessage('Title must not be empty.')
+    .isString()
+    .withMessage('title must be string.')
     .isLength({ min: 2, max: 20 })
-    .withMessage('Title must be between 2 and 20 characters long.'),
+    .withMessage('title must be between 2 and 20 characters long.'),
   body('content')
-    .notEmpty()
-    .withMessage('Content must not be empty.')
-    .isLength({ min: 2 })
-    .withMessage('Title must be at least 2 characters long.'),
+    .isString()
+    .withMessage('content must be string.')
+    .isLength({ max: 300 })
+    .withMessage('content must be no more than 300 characters long.'),
+  body('userId').isUUID().withMessage('userId must be UUID'),
+];
+
+export const noteIdValidator = [
+  param('id').isUUID().withMessage('id must be valid'),
+];
+
+export const updateNoteValidator = [
+  body('title')
+    .optional()
+    .isString()
+    .withMessage('title must be string.')
+    .isLength({ min: 2, max: 20 })
+    .withMessage('title must be between 2 and 20 characters long.'),
+  body('content')
+    .optional()
+    .isString()
+    .withMessage('content must be string.')
+    .isLength({ max: 300 })
+    .withMessage('content must be no more than 300 characters long.'),
+  body('userId').optional().isUUID().withMessage('userId must be UUID'),
 ];
--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -1,14 +1,48 @@
-import { body } from 'express-validator';
+import { body, param } from 'express-validator';
 
 export const createUserValidator = [
   body('name')
     .notEmpty()
-    .withMessage('Name must not be empty.')
+    .withMessage('name must not be empty.')
+    .isString()
+    .withMessage('name must be string.')
     .isLength({ min: 2, max: 10 })
-    .withMessage('Name must be between 2 and 10 characters long.'),
+    .withMessage('name must be between 2 and 10 characters long.'),
   body('email')
     .notEmpty()
-    .withMessage('Email must not be empty.')
+    .withMessage('email must not be empty.')
     .isEmail()
-    .withMessage('Email must be valid.'),
+    .withMessage('email must be valid.'),
+  body('password')
+    .isString()
+    .withMessage('name must be string.')
+    .notEmpty()
+    .withMessage('password must not be empty.'),
+];
+
+export const userIdValidator = [
+  param('id').isUUID().withMessage('id must be valid'),
+];
+
+export const updateUserValidator = [
+  body('name')
+    .optional()
+    .notEmpty()
+    .withMessage('name must not be empty.')
+    .isString()
+    .withMessage('name must be string.')
+    .isLength({ min: 2, max: 10 })
+    .withMessage('name must be between 2 and 10 characters long.'),
+  body('email')
+    .optional()
+    .notEmpty()
+    .withMessage('email must not be empty.')
+    .isEmail()
+    .withMessage('email must be valid.'),
+  body('password')
+    .optional()
+    .isString()
+    .withMessage('name must be string.')
+    .notEmpty()
+    .withMessage('password must not be empty.'),
 ];
