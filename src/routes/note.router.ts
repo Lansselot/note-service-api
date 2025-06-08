7b734b2b6bf7cccc3988db9bea7c50fd5ea9a1fd
lansselot <pozharov2003@gmail.com>
2025-06-08 03:11:30 +0300

    add routers

---
+++ src/routes/note.router.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { NoteController } from '../controllers/note.controller';
+import {
+  createNoteValidator,
+  noteIdValidator,
+  updateNoteValidator,
+} from '../validators/note.validator';
+import { validate } from '../middleware/validate';
+
+const router = Router();
+const noteController = new NoteController();
+
+router.post('/', createNoteValidator, validate, noteController.createNote);
+router.get('/', noteController.getAllNotes);
+router.get('/:id', noteIdValidator, validate, noteController.getNoteById);
+router.put(
+  '/:id',
+  createNoteValidator,
+  noteIdValidator,
+  validate,
+  noteController.updateNote
+);
+router.patch(
+  '/:id',
+  updateNoteValidator,
+  noteIdValidator,
+  validate,
+  noteController.updateNote
+);
+router.delete('/:id', noteIdValidator, validate, noteController.deleteNote);
+
+export default router;
---
+++ src/routes/user.router.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import {
+  createUserValidator,
+  userIdValidator,
+  updateUserValidator,
+} from '../validators/user.validator';
+import { validate } from '../middleware/validate';
+import { UserController } from '../controllers/user.controller';
+
+const router = Router();
+const userController = new UserController();
+
+router.post('/', createUserValidator, validate, userController.createUser);
+router.get('/', userController.getAllUsers);
+router.get('/:id', userIdValidator, validate, userController.getUserById);
+router.put(
+  '/:id',
+  userIdValidator,
+  createUserValidator,
+  validate,
+  userController.updateUser
+);
+router.patch(
+  '/:id',
+  userIdValidator,
+  updateUserValidator,
+  validate,
+  userController.updateUser
+);
+router.delete('/:id', userIdValidator, validate, userController.deleteUser);
+
+export default router;
