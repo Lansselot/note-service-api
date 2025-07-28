90180ba4ee225278990b0e281cee1102f862791a
lansselot <pozharov2003@gmail.com>
2025-07-28 16:57:05 +0300

    add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import { authRoutes, noteRoutes, userRoutes } from './routes';
 import { errorHandler } from './middleware/error-handler.middleware';
+import swaggerDocs from './utils/swagger';
 
 export const app: Express = express();
 
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
+
+swaggerDocs(app);
 
 app.use('/', (_, res) => {
   res.status(404).json({ message: 'Not found' });
