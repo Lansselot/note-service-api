5f94c061eade03df584d50055779daa90be16a7a
lansselot <pozharov2003@gmail.com>
2025-07-29 23:22:05 +0300

    add refresh token rotation

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,6 +3,8 @@ import { userService } from '.';
 import bcrypt from 'bcryptjs';
 import { AppJwtPayload, JwtTokens } from '../types/jwt';
 import { generateTokens, verifyRefreshToken } from '../utils/jwt';
+import '../redis-client';
+import redis from '../redis-client';
 
 export class AuthService {
   async login(email: string, password: string): Promise<JwtTokens> {
@@ -14,14 +16,37 @@ export class AuthService {
 
     const payload: AppJwtPayload = { userId: user.id };
     const tokens = generateTokens(payload);
+
+    await redis.set(
+      `refresh:${user.id}:${tokens.refreshToken}`,
+      1,
+      'EX',
+      60 * 60 * 24
+    );
+
     return tokens;
   }
 
-  async refresh(refreshToken: string): Promise<JwtTokens> {
-    const decoted = verifyRefreshToken(refreshToken);
+  async refresh(oldRefreshToken: string): Promise<JwtTokens> {
+    const decoted = verifyRefreshToken(oldRefreshToken);
+    const storedRefreshToken = await redis.exists(
+      `refresh:${decoted.userId}:${oldRefreshToken}`
+    );
+
+    if (!storedRefreshToken) throw Boom.unauthorized('Invalid refresh token');
+
+    await redis.del(`refresh:${decoted.userId}:${oldRefreshToken}`);
 
     const payload: AppJwtPayload = { userId: decoted.userId };
     const tokens = generateTokens(payload);
+
+    await redis.set(
+      `refresh:${decoted.userId}:${tokens.refreshToken}`,
+      1,
+      'EX',
+      60 * 60 * 24
+    );
+
     return tokens;
   }
 }
